d0356d75f616c69bb872ffe9c41e84caa88b62fe
procurementfpf-source <procurementfpf@gmail.com>
2026-08-21 11:45:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
    Bump CACHE when you publish a new version of index.html. */
 
-const CACHE = 'order-tracker-v45-1';
+const CACHE = 'order-tracker-v45-2';
 const SHELL = [
   './',
   './index.html',
